20dc6f0c56cac9c7922682fc6db91901431b6ffb
abdelaziz-sekouti <abdelaziz-sekouti@hotmail.com>
2026-02-13 17:51:29 +0100

    3 last images added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"db373eb85a06eb3d706af362a45d42d0972f4b
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2956921715"
+    serviceWorkerVersion: "2251248060"
   }
 });
